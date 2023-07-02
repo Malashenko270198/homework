ec4373bc16105cf369a679cdcb2346f3a141d4eb
Ruslan Malashenko <malashencko.ruslan@yandex.ru>
2023-07-02 10:27:35 +0500

    add func_2

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
 def func(function):
+    """
+    :param function:
+    :return:
+    """
     return function.title()
+
+
+def func_2(function):
+    """
+    :param function:
+    :return:
+    """
+    return function.upper()
